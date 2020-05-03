ebbc2dddeb4a9324f6a24714d6248142f352dad5
larry <dh821020@gmail.com>
2020-05-04 02:36:07 +0900

    second-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'pages#index'
+  get "/users",to: redirect('users/sign_up')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
